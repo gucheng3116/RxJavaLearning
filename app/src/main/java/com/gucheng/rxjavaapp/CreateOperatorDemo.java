e23d38bcf4146fe734e04fae60b8118177d13b47
gucheng <459843397@qq.com>
2021-12-21 22:07:37 +0800

    add rxjava learning

---
+++ app/src/main/java/com/gucheng/rxjavaapp/CreateOperatorDemo.java
@@ -0,0 +1,97 @@
+package com.gucheng.rxjavaapp;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+
+/**
+ * Created on 2021/12/21.
+ */
+public class CreateOperatorDemo {
+//    public static void main(String[] args) {
+//        System.out.println("====================");
+//        test1();
+//        System.out.println("====================");
+//    }
+
+    public static void test1() {
+        Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Object> emitter) throws Throwable {
+                //事件产生的地方
+                emitter.onNext("1");
+                emitter.onNext("2");
+                emitter.onNext("1");
+                emitter.onNext("3");
+                emitter.onNext("6");
+//                emitter.onComplete();
+                emitter.onError(new Throwable("test error"));
+
+            }
+        }).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(Object o) throws Throwable {
+                System.out.println("accept1... " + o);
+            }
+
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Throwable {
+                System.out.println("accept2..." + throwable);
+            }
+        });
+    }
+
+    public static void test2() {
+        Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Object> emitter) throws Throwable {
+                //事件产生的地方
+                emitter.onNext("1");
+                emitter.onNext("2");
+                emitter.onNext("1");
+                emitter.onNext("3");
+                emitter.onNext("6");
+//                emitter.onComplete();
+                emitter.onError(new Throwable("test error"));
+
+            }
+        }).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(Object o) throws Throwable {
+                System.out.println("accept1... " + o);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Throwable {
+                System.out.println("accept2..." + throwable);
+            }
+        });
+    }
+
+    Observer obeserver = new Observer<Object>() {
+        @Override
+        public void onSubscribe(@NonNull Disposable d) {
+
+        }
+
+        @Override
+        public void onNext(@NonNull Object o) {
+
+        }
+
+        @Override
+        public void onError(@NonNull Throwable e) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/gucheng/rxjavaapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gucheng.rxjavaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CreateOperatorDemo.test1();
+    }
+}
